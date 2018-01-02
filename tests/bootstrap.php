ea5e9938d38adee17f0e3b4e730b1050a9c425c9
Joe Nilson <joenilson@gmail.com>
2018-01-02 12:45:59 -0400

    yggdrasil: added phpunit initial configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Copyright (C) 2018 Joe Nilson <joenilson at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+if (isset($_ENV['BOOTSTRAP_CLEAR_CACHE_ENV'])) {
+    // executes the "php bin/console cache:clear" command
+    passthru(sprintf(
+        'php "%s/../bin/console" cache:clear --env=%s --no-warmup',
+        __DIR__,
+        $_ENV['BOOTSTRAP_CLEAR_CACHE_ENV']
+    ));
+}
+
+require __DIR__.'/../vendor/autoload.php';
