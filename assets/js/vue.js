1b81f1cd5baf92876a69978cc5ad6364b7924a99
Joe Nilson <joenilson@gmail.com>
2018-01-05 17:28:34 -0400

    yggdrasil: Fixed css on base.html.twig and index.html.twig

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -25,27 +25,14 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 Vue.use(Buefy, {
-    //defaultIconPack: 'fa',
+    defaultIconPack: 'fa'
 });
 //Vue.use(VueRouter);
 
 // bootstrap the vueApp
 let yggdrasil = new Vue({
-  el: '#vueApp',
-  data: {
-        tableData: [
-            {"id":1,"user":{"first_name":"Jesse","last_name":"Simmons"},"date":"2016-10-15 13:43:27","gender":"Male"},
-            {"id":2,"user":{"first_name":"John","last_name":"Jacobs"},"date":"2016-12-15 06:00:53","gender":"Male"},
-            {"id":3,"user":{"first_name":"Tina","last_name":"Gilbert"},"date":"2016-04-26 06:26:28","gender":"Female"},
-            {"id":4,"user":{"first_name":"Clarence","last_name":"Flores"},"date":"2016-04-10 10:28:46","gender":"Male"},
-            {"id":5,"user":{"first_name":"Anne","last_name":"Lee"},"date":"2016-12-06 14:38:38","gender":"Female"},
-            {"id":6,"user":{"first_name":"Sara","last_name":"Armstrong"},"date":"2016-09-23 18:50:04","gender":"Female"},
-            {"id":7,"user":{"first_name":"Anthony","last_name":"Webb"},"date":"2016-08-30 23:49:38","gender":"Male"},
-            {"id":8,"user":{"first_name":"Andrew","last_name":"Greene"},"date":"2016-11-20 14:57:47","gender":"Male"},
-            {"id":9,"user":{"first_name":"Russell","last_name":"White"},"date":"2016-07-13 09:29:49","gender":"Male"},
-            {"id":10,"user":{"first_name":"Lori","last_name":"Hunter"},"date":"2016-12-09 01:44:05","gender":"Female"}
-        ]
-    }
+  el: '#vueApp'
+
   //,template: '<App/>',
   //components: { App }
 });
