4f480e18ee298c5a2c2dd09a91a4bd5eb23054cf
Joe Nilson <joenilson@gmail.com>
2018-01-09 00:22:05 -0400

    yggdrasil: Added Spain and Perú maps to test datamaps functionality

--- assets/js/components/dashboardMap.vue
+++ assets/js/components/dashboardMap.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="tile is-parent is-6">
+<div class="tile is-parent is-9">
   <div class="tile is-child box">
     <div class="card">
       <header class="card-header">
@@ -18,16 +18,18 @@
         </div>
       </div>
       <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
+        <a map_country="esp" href="#" class="card-footer-item" @click="showMap">España</a>
+        <a map_country="dom" href="#" class="card-footer-item" @click="showMap">República Dominicana</a>
+        <a map_country="per" href="#" class="card-footer-item" @click="showMap">Perú</a>
       </footer>
     </div>
   </div>
 </div>
 </template>
 <script>
-import * as Datamap from './../../vendors/datamaps/datamaps.dom'
+import * as DatamapDom from './../../vendors/datamaps/datamaps.dom'
+import * as DatamapEsp from './../../vendors/datamaps/datamaps.esp'
+import * as DatamapPer from './../../vendors/datamaps/datamaps.per'
 let map;
 export default {
   name: 'dashboard-map',
@@ -40,12 +42,24 @@ export default {
       this.map.resize();
     },
     showMap() {
+      console.log(this.map_country);
       var map_div = document.getElementById('map-content');
       if(map_div.firstChild){
         map_div.removeChild(map_div.firstChild);
+        map_div.removeChild(map_div.lastChild);
+      }
+      var Datamap = DatamapDom;
+      var map_center = [-70.41667, 18.93333];
+      if(this.map_country === 'esp') {
+        Datamap = DatamapEsp;
+        map_center = [-3.7025600,40.4165000];
+      }
+      if(this.map_country === 'per') {
+        Datamap = DatamapPer;
+        map_center = [-76.0000000,-10.0000000];
       }
       this.map = new Datamap({
-        scope: 'dom',
+        scope: this.map_country,
         element: document.getElementById('map-content'),
         responsive: true,
         fills: {
@@ -54,8 +68,8 @@ export default {
         setProjection: function(element, options) {
           var projection, path;
           projection = d3.geo.equirectangular()
-            .center([-70.41667, 18.93333])
-            .scale(element.offsetWidth * 10)
+            .center(map_center)
+            .scale(element.offsetWidth * 1.5)
             .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
           path = d3.geo.path()
             .projection(projection);
